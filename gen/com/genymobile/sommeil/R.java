ced2b4974732a346ef1ac35bd6a23055c9d05939
mfportzert <mfportzert@genymobile.com>
2012-12-08 08:43:55 +0100

    commit all branch

--- gen/com/genymobile/sommeil/R.java
+++ gen/com/genymobile/sommeil/R.java
@@ -14,10 +14,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int lightSeekBar=0x7f070001;
+        public static final int lightSeekBar2=0x7f070003;
+        public static final int menu_settings=0x7f070004;
+        public static final int toggleLight=0x7f070000;
+        public static final int toggleLight2=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
